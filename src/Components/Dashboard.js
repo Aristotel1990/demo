b7bb04b960337257b0f10a728386b5dd1ef64117
teli <aristotel.dhrako@gmail.com>
2021-06-16 11:16:23 +0200

    third

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,44 +3,44 @@ import Table from 'react-bootstrap/Table'
 import { useStateValue } from "../reducers/StateProvider";
 
 function Dashboard() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
-    return (
-        <div>
-       
-       
-            <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>Nr</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Address</th>
-      <th>Address 2 </th>
-      <th>Age</th>
-      
+  return (
+    <div>
 
-    </tr>
-  </thead>
-  <tbody>
-    {basket.map((data)=>
-    ( <tr>
 
-      <td>{data.id}</td>
-      <td>{data.name}</td>
-      <td>{data.surname}</td>
-      <td>{data.address}</td>
-      <td>{data.address2}</td>
-      <td>{data.age}</td>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nr</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Address</th>
+            <th>Address 2 </th>
+            <th>Age</th>
 
-    </tr>)
-    )}
-   
-    
-  </tbody>
-</Table>
-        </div>
-    )
+
+          </tr>
+        </thead>
+        <tbody>
+          {basket.map((data) =>
+          (<tr>
+
+            <td>{data.id}</td>
+            <td>{data.name}</td>
+            <td>{data.surname}</td>
+            <td>{data.address}</td>
+            <td>{data.address2}</td>
+            <td>{data.age}</td>
+
+          </tr>)
+          )}
+
+
+        </tbody>
+      </Table>
+    </div>
+  )
 }
 
 export default Dashboard
